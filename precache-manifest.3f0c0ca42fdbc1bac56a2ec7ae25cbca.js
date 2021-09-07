cb36df0bb2da406e4897e6c3109d3bb871bdf9fc
Ruan Miguel <ruanmiguelps@gmail.com>
2021-09-07 10:17:47 -0300

    Updates

---
+++ precache-manifest.3f0c0ca42fdbc1bac56a2ec7ae25cbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6089ef9abce32466f224498f02fe56",
+    "url": "/MovieCards/index.html"
+  },
+  {
+    "revision": "99135c2d797e73c8d696",
+    "url": "/MovieCards/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "5bba4c317ed687a3dd1c",
+    "url": "/MovieCards/static/js/2.943014cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieCards/static/js/2.943014cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99135c2d797e73c8d696",
+    "url": "/MovieCards/static/js/main.9d8a73e0.chunk.js"
+  },
+  {
+    "revision": "243b2fcccd19bb139ae5",
+    "url": "/MovieCards/static/js/runtime-main.d9ae80a2.js"
+  }
+]);
